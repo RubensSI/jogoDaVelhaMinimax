1c3da9495d468d27f547d6671781f76e8f75acdc
Rubens Santos <rubenssi825@gmail.com>
2022-03-15 16:29:12 -0300

    adicionei alguns comentários ao código

--- jogo.py
+++ jogo.py
@@ -1,7 +1,10 @@
 # sempre vai ter uma string vazia
 ini = " "
+
+# Token do jogadores
 token = ["X", "O"]
 
+# Criando o tabuleiro
 def criar_board():
     board = [
         [ini, ini, ini],
@@ -10,13 +13,18 @@ def criar_board():
     ]
     return board
 
+# Imprimindo o Tabuleiro
 def print_board(board):
+    # Usando o metodo join para pegar todos os itens em um iteravél
+    # e unindo em uma string tendo o pipe como separador
     for i in range(3):
         print(" | ".join(board[i]))
         if (i < 2 ):
             print("----------")
     print()
 
+# Obtendo os inputs validos, ou seja,
+# Pegando a linha e coluna do jogador
 def get_input_valid(msg):
     try:
         n = int(input(msg))
@@ -29,16 +37,22 @@ def get_input_valid(msg):
         print("numero não válido")
         return get_input_valid(msg)
 
+# Verificando se o movimento é valido
 def verifica_movimento(board, i, j):
+    # Se as posições do tabuleiro for igual a vazio ele retorna True
+    # Ou seja a posição está disponivel
     if(board[i][j] == ini):
         return True
     else:
         return False
 
+# Se movendo
 def fazer_movimento(board, i, j, jogador):
     board[i][j] = token[jogador]
 
+# Verificando quem ganhou
 def verifica_ganhador(board):
+
      # horizontal
      for i in range(3):
          if (board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != ini):
@@ -57,6 +71,8 @@ def verifica_ganhador(board):
      if (board[0][2] != ini and board[0][2] == board[1][1] and board[1][1] == board[2][0]):
         return board[0][2]
 
+     # Percorrendo o taboleiro atras de um campo em branco e caso tenha, os jagadores irão jogar novamente
+     # Caso não tenha nenhum campo vazio ele sai do for e retorna empate
      for i in range(3):
          for j in range(3):
              if(board[i][j] == ini):
--- minimax.py
+++ minimax.py
@@ -1,29 +1,45 @@
 from jogo import *
 
 def movimento_ia(board, jogador):
+    # Verificar todas as possibilidades
     possibilidades = getPosicoes(board)
     melhor_valor = None
     melhor_movimento = None
 
+    # Considerando a melhor possibilidade para ganhar o jogo
+    # Vamos descobrir o melhor movimento
+    # pesquisando em todas as possibilidades
     for possibilidade in possibilidades:
+        # O tabuleiro irá sofrer uma mudança
+        # Acessando assim a nova lista de posições
+        # Setando temporariamente assim a 'O' ou 'X'
         board[possibilidade[0]][possibilidade[1]] = token[jogador]
+        # Retornano o valor do miniMax que é o '0', '1' e o '-1'
         valor = minimax(board, jogador)
+        # Limpando o tabuleiro
         board[possibilidade[0]][possibilidade[1]] = ini
+        print(possibilidade, valor)
 
+        # Verificando se existe um melhor valor já setado na variavel
         if(melhor_valor is None):
-            valor = melhor_valor
+            melhor_valor = valor
             melhor_movimento = possibilidade
+            # Aqui a 'O' quer sempre o maior valor
         elif(jogador == 0):
             if(valor > melhor_valor):
-                valor = melhor_valor
+                melhor_valor = valor
                 melhor_movimento = possibilidade
         elif( jogador == 1):
+            # Aqui o 'X' quer sempre o menor valor
             if(valor < melhor_valor):
                 valor = melhor_valor
                 melhor_movimento = possibilidade
-
+    print("Probabilidade de vitoria:", melhor_valor * 100, "%")
+    # Acessando a lista de possibilidade da variável posições
+    # Retornar o melhor movimento
     return melhor_movimento[0], melhor_movimento[1]
 
+# O getPosicoes pega todas as posições vazias
 def getPosicoes(board):
     posicoes = []
     for i in range(3):
@@ -40,25 +56,44 @@ pontos = {
 
 
 def minimax(board, jogador):
+    # Aqui ele recebe o resultado  False ou True se houver um ganhador
     ganhador = verifica_ganhador(board)
     if (ganhador):
+        # Se existir um ganhador
+        # pontos recebe o ganhador e logo depois vai acessar o
+        # dicionario de dados contendo chave e valor
+        # pegando  o valor referente aquela chave 'X' , 'O' ou 'EMPATE'.
         return pontos[ganhador]
+    # Troco os jogadores
     jogador = (jogador + 1) % 2
 
+    # A maquina vai testar as possibilidade, ou seja, as  jogadas do oponete
     possibilidades = getPosicoes(board)
     melhor_valor = None
+
     for possibilidade in possibilidades:
+        # O tabuleiro irá sofrer uma mudança
+        # Acessando assim a nova lista de posições
+        # Setando assim a 'O' ou 'X
         board[possibilidade[0]][possibilidade[1]] = token[jogador]
+        # Retornano o valor do minimax que pode ser '0', '1' e o '-1'
         valor = minimax(board, jogador)
+        # limpando o tabuleiro
         board[possibilidade[0]][possibilidade[1]] = ini
 
+        # Verificando se existe um melhor valor já setado na variavel
         if (melhor_valor is None):
             melhor_valor = valor
+            # Aqui a 'O' quer sempre o maior valor
+            # Vai verificar se o jagador é a vez da máquina
         elif (jogador == 0):
             if (valor > melhor_valor):
                 melhor_valor = valor
+        # Aqui o 'X' quer sempre o menor valor
+        # Vai verificar se o jagador é o jogador é o X
         elif (jogador == 1):
             if (valor < melhor_valor):
                 melhor_valor = valor
 
+    # Retorna o melhor valor caso ele não entre em nenhuma destas condições
     return melhor_valor
